f51730c609363c1dbe2b97a519ed949fa4ecfca8
Mind Twisted <nikolaynikm@gmail.com>
2017-12-23 08:51:41 +0200

    user seeder changed

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,8 +2,8 @@
 
 $factory->define(App\User::class, function () {
     return [
-        'name' => 'MindTwisted',
-        'email' => 'nikolaynikm@gmail.com',
+        'name' => 'TestUser',
+        'email' => 'test@example.com',
         'password' => bcrypt('secret'),
         'remember_token' => str_random(10),
     ];
