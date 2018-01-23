37b99c64440d9a6626ce5148b2f1baec4731b76a
Mind Twisted <nikolaynikm@gmail.com>
2018-01-23 21:05:46 +0200

    site logo home link added

--- resources/site/components/logo/logo.blade.php
+++ resources/site/components/logo/logo.blade.php
@@ -1,8 +1,9 @@
-<div class="logo {{ $secondary or false ? 'logo--secondary' : '' }}">
+<a href="/"
+   class="logo {{ $secondary or false ? 'logo--secondary' : '' }}">
   <img src="/images/landing/logo.png"
        alt="Expire Logo"
        class="logo__image">
   <div class="logo__text">
     Expire
   </div>
-</div>
+</a>
