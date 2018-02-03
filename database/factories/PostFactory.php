3ac67eb6cac72d2dbcff817366c5a8fd1c503062
Mind Twisted <nikolaynikm@gmail.com>
2018-02-03 18:28:22 +0200

    posts seeder fix: generate timestamp randomly for last year

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -3,9 +3,13 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Post::class, function (Faker $faker) {
+    $date = $faker->dateTimeThisYear;
+
     return [
         'title' => $faker->words($nb = 3, $asText = true),
         'body' => $faker->text($maxNbChars = 5000),
-        'body_preview' => $faker->text($maxNbChars = 2000)
+        'body_preview' => $faker->text($maxNbChars = 2000),
+        'created_at' => $date,
+        'updated_at' => $date
     ];
 });
